885e6a85f321a5254e156f4372233ed090934fa0
Gauransh <gauranshgupta99@gmail.com>
2022-12-23 14:10:42 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:mark_my_attendance/dbHelper/MongoDbStudent.dart';
 import 'package:mark_my_attendance/dbHelper/mongodb.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import './theme/colors.dart';
 import './screens/select_branch.dart';
 import './utils/names.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +18,9 @@ Future main() async {
   await StudentMongoDB.connect();
   await MongoDataBase.connect();
   await StudentMongoDB.getStudentData();
+//   await Firebase.initializeApp(
+//     options: DefaultFirebaseOptions.currentPlatform,
+// );
   print(date);
   runApp(MyApp());
 }
@@ -28,8 +33,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var themeData = ThemeData(
-        primarySwatch: primaryColor,
-        dividerColor: Colors.black);
+        primarySwatch: newprimaryColor,
+        dividerColor: Colors.white);
     // DE0000
     return MaterialApp(
       debugShowCheckedModeBanner: false,
